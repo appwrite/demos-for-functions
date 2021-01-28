ed83c063095e19ee37a00d2049acdca975b05df1
Torsten Dittmann <torsten.dittmann@googlemail.com>
2021-01-28 12:41:56 +0100

    adds dotnet examples

---
+++ dotnet/template/Program.cs
@@ -0,0 +1,25 @@
+﻿using Appwrite;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace template
+{
+  class Program
+  {
+    static async Task Main(string[] args)
+    {
+      Client client = new Client();
+      client
+        .SetEndPoint("http://localhost:9501/v1") // Your API Endpoint 
+        .SetProject("5df5acd0d48c2") // Your project ID 
+        .SetKey("919c2d18fb5d4...a2ae413da83346ad2") // Your secret API key 
+      ;
+
+      Storage storage = new Storage(client);
+      HttpResponseMessage result = await storage.ListFiles();
+      
+      Console.WriteLine(result.ToString());
+    }
+  }
+}
---
+++ dotnet/welcome-email/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+
+namespace welcome_email
+{
+  class Program
+  {
+    static HttpClient client = new HttpClient();
+    static async Task Main(string[] args)
+    {
+      string url = $"https://api.mailgun.net/v3/{Environment.GetEnvironmentVariable("MAILGUN_DOMAIN")}/messages";
+      string base64authorization = Convert.ToBase64String(Encoding.ASCII.GetBytes($"api:{Environment.GetEnvironmentVariable("MAILGUN_API_KEY")}"));
+      HttpRequestMessage request = new HttpRequestMessage(new HttpMethod("POST"), url);
+      request.Headers.TryAddWithoutValidation("Authorization", $"Basic {base64authorization}");
+
+      User user = JsonSerializer.Deserialize<User>(Environment.GetEnvironmentVariable("APPWRITE_FUNCTION_EVENT_PAYLOAD"));
+
+      MultipartFormDataContent multipartContent = new MultipartFormDataContent();
+      multipartContent.Add(new StringContent("Mailgun Sandbox <welcome@my-awesome-app.io>"), "from");
+      multipartContent.Add(new StringContent(user.email), "to");
+      multipartContent.Add(new StringContent($"Welcome on board {user.name}!"), "subject");
+      multipartContent.Add(new StringContent($"Hi {user.name}\nGreat to have you with us!😍"), "text");
+      request.Content = multipartContent;
+
+      HttpResponseMessage response = await client.SendAsync(request);
+      Console.WriteLine(response);
+    }
+  }
+  public class User
+  {
+    public string name
+    {
+      get;
+      set;
+    }
+    public string email
+    {
+      get;
+      set;
+    }
+  }
+}
