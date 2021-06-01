b97b6ca9aaca9e926a30abcb58cdc12618816000
Damodar Lohani <dlohani48@gmail.com>
2021-06-01 13:40:01 +0545

    upgrade dart functions

--- dart/storage_cleaner/main.dart
+++ dart/storage_cleaner/main.dart
@@ -7,15 +7,15 @@ void main(List<String> args) async {
   Map<String, String> envVars = Platform.environment;
   final Client client = Client();
   client
-      .setEndpoint(envVars['APPWRITE_ENDPOINT'])
-      .setProject(envVars['APPWRITE_PROJECT_ID'])
+      .setEndpoint(envVars['APPWRITE_ENDPOINT'] ?? 'https://localhost/v1') // This is manually set
+      .setProject(envVars['APPWRITE_FUNCTION_PROJECT_ID']) // this is available by default
       .setKey(
           envVars['APPWRITE_API_KEY']);
 
   // Initialise the storage SDK
   final storage = new Storage(client);
 
-  int daysToExpire = int.parse(envVars['DAYS_TO_EXPIRE']);
+  int daysToExpire = int.parse(envVars['DAYS_TO_EXPIRE'] ?? "100");
 
   final res = await storage.listFiles(orderType: OrderType.desc, limit: 100);
   final data = res.data;
