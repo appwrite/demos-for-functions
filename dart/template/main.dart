4001b0d36a11a59992b63cafca25f64e6c101c71
Damodar Lohani <dlohani48@gmail.com>
2021-06-01 14:10:53 +0545

    get project id by default

--- dart/template/main.dart
+++ dart/template/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:dart_appwrite/dart_appwrite.dart';
 
 void main(List<String> args) {
@@ -5,7 +7,7 @@ void main(List<String> args) {
   final Client client = Client();
   client
           .setEndpoint('https://[HOSTNAME_OR_IP]/v1') // Your API Endpoint
-          .setProject('5df5acd0d48c2') // Your project ID
+          .setProject(Platform.environment['APPWRITE_FUNCTION_PROJECT_ID']) // Your project ID
           .setKey('919c2d18fb5d4...a2ae413da83346ad2') // Your secret API key
       ;
 
