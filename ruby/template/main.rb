ee94668fb6c603e2089417152399a316d0a93b09
Damodar Lohani <dlohani48@gmail.com>
2021-06-01 14:17:42 +0545

    upgrade ruby dependency

--- ruby/template/main.rb
+++ ruby/template/main.rb
@@ -4,7 +4,7 @@ client = Appwrite::Client.new()
 
 client
     .set_endpoint('http://[HOSTNAME_OR_IP]/v1') # Your API Endpoint
-    .set_project('5fca866c65afc') # Your project ID
+    .set_project(ENV.APPWRITE_FUNCTION_PROJECT_ID) # Your project ID available by default
     .set_key('8d84bc37d4c59bb3b3e9d...a9f1ab664e94e6df53') # Your secret API key
 ;
 
