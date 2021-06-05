2c20d1f24cc605fb0159845d39f42acdb536e20e
Fixirk <52279350+Fixirk@users.noreply.github.com>
2021-06-05 16:01:08 +0200

    fix: check if APPWRITE_FUNCTION_EVENT_DATA and file ID are defined to prevent errors during execution

--- nodejs/object-detection/index.js
+++ nodejs/object-detection/index.js
@@ -11,37 +11,47 @@ const APPWRITE_PROJECT_ID = process.env.APPWRITE_PROJECT_ID;
 const APPWRITE_API_KEY = process.env.APPWRITE_API_KEY;
 
 // Contains the data related to the storage.files.create event
-const APPWRITE_FUNCTION_EVENT_DATA = process.env.APPWRITE_FUNCTION_EVENT_DATA;
-const { $id: fileID } = JSON.parse(APPWRITE_FUNCTION_EVENT_DATA);
+const APPWRITE_FUNCTION_EVENT_DATA = JSON.parse(
+  process.env.APPWRITE_FUNCTION_EVENT_DATA
+);
 
-// Configure the AppWrite SDK
-const appwriteClient = new sdk.Client();
-appwriteClient
-  .setEndpoint(APPWRITE_ENDPOINT)
-  .setProject(APPWRITE_PROJECT_ID)
-  .setKey(APPWRITE_API_KEY);
+if (APPWRITE_FUNCTION_EVENT_DATA && APPWRITE_FUNCTION_EVENT_DATA.$id) {
+  const { $id: fileID } = APPWRITE_FUNCTION_EVENT_DATA;
 
-const storage = new sdk.Storage(appwriteClient);
+  // Configure the AppWrite SDK
+  const appwriteClient = new sdk.Client();
+  appwriteClient
+    .setEndpoint(APPWRITE_ENDPOINT)
+    .setProject(APPWRITE_PROJECT_ID)
+    .setKey(APPWRITE_API_KEY);
 
-// Configure the CloudMersive client
-const cloudmersiveDefaultClient = CloudmersiveImageApiClient.ApiClient.instance;
-cloudmersiveDefaultClient.authentications.Apikey.apiKey = CLOUDMERSIVE_API_KEY;
+  const storage = new sdk.Storage(appwriteClient);
 
-const cloudmersiveRecognizeApi = new CloudmersiveImageApiClient.RecognizeApi();
+  // Configure the CloudMersive client
+  const cloudmersiveDefaultClient =
+    CloudmersiveImageApiClient.ApiClient.instance;
+  cloudmersiveDefaultClient.authentications.Apikey.apiKey =
+    CLOUDMERSIVE_API_KEY;
 
-// Get the image file
-storage
-  .getFilePreview(fileID)
-  .then((file) => {
-    // Recognize objects in the retrieved image
-    cloudmersiveRecognizeApi.recognizeDetectObjects(file, (error, res) => {
-      if (error) {
-        console.error('Exception when calling CloudmersiveRecognizeApi', e);
-      } else {
-        console.log(res);
-      }
+  const cloudmersiveRecognizeApi =
+    new CloudmersiveImageApiClient.RecognizeApi();
+
+  // Get the image file
+  storage
+    .getFilePreview(fileID)
+    .then((file) => {
+      // Recognize objects in the retrieved image
+      cloudmersiveRecognizeApi.recognizeDetectObjects(file, (error, res) => {
+        if (error) {
+          console.error('Exception when calling CloudmersiveRecognizeApi', e);
+        } else {
+          console.log(res);
+        }
+      });
+    })
+    .catch((e) => {
+      console.error('An error occured during fetch of image preview', e);
     });
-  })
-  .catch((e) => {
-    console.error('An error occured during fetch of image preview', e);
-  });
+} else {
+  console.error('File ID is undefined');
+}
