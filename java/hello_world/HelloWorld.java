e013ff526e1deb708eeed9d529622d1714f1b6bc
Christy Jacob <christyjacob4@gmail.com>
2021-06-25 14:46:31 +0530

    feat: remove unrelated files

--- java/hello_world/HelloWorld.java
+++ java/hello_world/HelloWorld.java
@@ -1,5 +1,1 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello Java");
-    }
-}
+public class HelloWorld { public static void main(String[ ] args) { System.out.println("Hello Java");  }}
