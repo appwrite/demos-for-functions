2592c9194af799c3b4e074fad270c75b8d04ede3
Michal <michal.sierpinski@pm.me>
2021-07-11 12:02:20 +0200

    add hello world project

---
+++ dotnet/hello-world/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World, I'm an Appwrite cloud function written in .NET.");
+        }
+    }
+}
