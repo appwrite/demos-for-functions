2b31dc8edc7cfbd7c4c6c7d81d60afc8ec927e3c
Akash Gupta <akashgp9@gmail.com>
2021-10-03 07:00:48 +0530

    feat: create ruby cloud function to translate text

---
+++ ruby/translate-text/main.rb
@@ -0,0 +1,13 @@
+# Import the helper libraries
+require 'free_google_translate'
+require 'json'
+
+# Get the text, sourceLanguage and destinationLanguage from Appwrite's environment variable
+payload = JSON.parse(ENV['APPWRITE_FUNCTION_DATA'])
+text=payload["text"]
+sourceLanguage=payload["sourceLanguage"]
+destinationLanguage=payload["destinationLanguage"]
+
+# Translate your text
+translation = GoogleTranslate.translate from: sourceLanguage, to: destinationLanguage, text: text
+puts translation
