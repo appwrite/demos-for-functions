03dc79ddd5db8ca84f5c53563361396be3d709d5
Apurva Kansara <apurvakansara5@gmail.com>
2021-10-03 13:36:54 +0530

    Code optimized

--- php/storage-cleaner/index.php
+++ php/storage-cleaner/index.php
@@ -3,20 +3,14 @@
 include './vendor/autoload.php';
 use Appwrite\Client;
 use Appwrite\Services\Storage;
-
-
 $client = new Client();
 $client
     ->setEndpoint($_ENV['APPWRITE_ENDPOINT'])
     ->setProject($_ENV['APPWRITE_FUNCTION_PROJECT_ID']) //Available by default
-    ->setKey($_ENV['APPWRITE_API_KEY'])
-;
-
+    ->setKey($_ENV['APPWRITE_API_KEY']);
 $storage = new Storage($client);
 $result = $storage->listFiles($orderType = 'DESC', $limit = 100);
-
 $timestamp = date_create()->modify("-{$_ENV['DAYS_TO_EXPIRE']} days")->getTimestamp();
-
 $deletedFiles = 0;
 foreach ($result['files'] as $file) {
     if ($file['dateCreated'] < $timestamp)
@@ -26,5 +20,4 @@ foreach ($result['files'] as $file) {
         print_r("Deleted {$file['$id']}");
     }
 }
-
 print_r("Total files deleted: {$deletedFiles}");
