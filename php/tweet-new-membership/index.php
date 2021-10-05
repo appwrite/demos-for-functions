f1e288159f29e4dc3a37074f81fbfdf654aec49f
Aditya Rana <official.adityarana01@gmail.com>
2021-10-05 22:03:43 +0530

    Removed unused import and moved the code into function.

--- php/tweet-new-membership/index.php
+++ php/tweet-new-membership/index.php
@@ -1,6 +1,5 @@
 <?php
 	include './vendor/autoload.php';
-	require_once './config.php';
 
 	use Appwrite\Client;
 
@@ -31,37 +30,40 @@
         return $r;
     }
 
-	// Twitter API v1.1
-    $url = "https://api.twitter.com/1.1/statuses/update.json";
-    $oauth = array( 'oauth_consumer_key' => $_ENV['TWITTER_CONSUMER_KEY'],
-                    'oauth_nonce' => time(),
-                    'oauth_signature_method' => 'HMAC-SHA1',
-                    'oauth_token' => $_ENV['TWITTER_ACCESS_TOKEN'],
-                    'oauth_timestamp' => time(),
-                    'oauth_version' => '1.0');
+    function tweetNewMembership() {
+        // Twitter API v1.1
+        $url = "https://api.twitter.com/1.1/statuses/update.json";
+        $oauth = array( 'oauth_consumer_key' => $_ENV['TWITTER_CONSUMER_KEY'],
+                        'oauth_nonce' => time(),
+                        'oauth_signature_method' => 'HMAC-SHA1',
+                        'oauth_token' => $_ENV['TWITTER_ACCESS_TOKEN'],
+                        'oauth_timestamp' => time(),
+                        'oauth_version' => '1.0');
 
-    $base_info = buildBaseString($url, 'POST', $oauth);
-    $composite_key = rawurlencode($_ENV['TWITTER_CONSUMER_SECRET']) . '&' . rawurlencode($_ENV['TWITTER_ACCESS_TOKEN_SECRET']);
-    $oauth_signature = base64_encode(hash_hmac('sha1', $base_info, $composite_key, true));
-    $oauth['oauth_signature'] = $oauth_signature;
+        $base_info = buildBaseString($url, 'POST', $oauth);
+        $composite_key = rawurlencode($_ENV['TWITTER_CONSUMER_SECRET']) . '&' . rawurlencode($_ENV['TWITTER_ACCESS_TOKEN_SECRET']);
+        $oauth_signature = base64_encode(hash_hmac('sha1', $base_info, $composite_key, true));
+        $oauth['oauth_signature'] = $oauth_signature;
 
-	$postfields = array("status"=> "A new user has joined the team! Welcome aboard!");
+        $postfields = array("status"=> "A new user has joined the team! Welcome aboard!");
 
-    // Make requests
-    $header = array(buildAuthorizationHeader($oauth), 'Expect:');
-    $options = array( CURLOPT_HTTPHEADER => $header,
-                      CURLOPT_POSTFIELDS => $postfields,
-                      CURLOPT_HEADER => false,
-                      CURLOPT_URL => $url);
+        // Make requests
+        $header = array(buildAuthorizationHeader($oauth), 'Expect:');
+        $options = array( CURLOPT_HTTPHEADER => $header,
+                        CURLOPT_POSTFIELDS => $postfields,
+                        CURLOPT_HEADER => false,
+                        CURLOPT_URL => $url);
 
-    $feed = curl_init();
-    curl_setopt_array($feed, $options);
-    $json = curl_exec($feed);
-    curl_close($feed);
+        $feed = curl_init();
+        curl_setopt_array($feed, $options);
+        $json = curl_exec($feed);
+        curl_close($feed);
 
-    $twitter_data = json_decode($json);
+        $twitter_data = json_decode($json);
 
-	// Printing response from twitter
-	print_r ($twitter_data);
+        // Printing response from twitter
+        print_r ($twitter_data);
+    }
 
+    tweetNewMembership();
 ?>
