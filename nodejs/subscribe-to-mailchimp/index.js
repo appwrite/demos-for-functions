deb63b8e776f4cd87a32cf5399f72556ede17003
Bence Hornyák <sir.bence15@gmail.com>
2021-10-05 22:42:56 +0200

    feat: demo function for mailchimp subscribe

---
+++ nodejs/subscribe-to-mailchimp/index.js
@@ -0,0 +1,16 @@
+const Mailchimp = require('mailchimp-api-v3');
+const listId = process.env.APPWRITE_MAILCHIMP_LIST_ID;
+const apiKey = process.env.APPWRITE_MAILCHIMP_API_KEY;
+const mailchimp = new Mailchimp(apiKey);
+
+const email = JSON.parse(process.env.APPWRITE_FUNCTION_DATA)['email'];
+
+const body = {
+    email_address: email,
+    status: 'subscribed'
+};
+
+mailchimp.post({
+    path: `/lists/${listId}/members`,
+    body
+}).then((response) => console.log(response)).catch((err) => console.error(err));
