71d58f23d99eaee2f7b8c57e1d2c21e3f9cdae8f
Saket Lakhotia <saket.lakhotia17@gmail.com>
2021-10-05 21:23:47 +0530

    Add files via upload

---
+++ python/hello-world/linkedlist.py
@@ -0,0 +1,16 @@
+# Node class
+class Node:
+
+	# Function to initialize the node object
+	def __init__(self, data):
+		self.data = data # Assign data
+		self.next = None # Initialize
+						# next as null
+
+# Linked List class
+class LinkedList:
+	
+	# Function to initialize the Linked
+	# List object
+	def __init__(self):
+		self.head = None
