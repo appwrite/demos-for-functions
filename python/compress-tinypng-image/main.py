850fad42b322bdf01eddc6e69f83fa4d5e3a143e
achintya <achintya22052000@gmail.com>
2021-10-06 21:57:54 +0530

    created main.py

---
+++ python/compress-tinypng-image/main.py
@@ -0,0 +1,29 @@
+from appwrite.client import Client
+from appwrite.services.storage import Storage
+
+import tinify
+
+import os
+
+client = Client()
+client.set_endpoint(os.environ["APPWRITE_ENDPOINT"]) 
+client.set_project(os.environ["APPWRITE_FUNCTION_PROJECT_ID"]) # this is available by default
+client.set_key(os.environ["APPWRITE_API_KEY"]) 
+
+
+
+def get_image():
+    tinify.key = os.environ["TINIFY_KEY"]
+    source = tinify.from_url(os.environ["IMAGE_URL"])
+    source.to_file("optimized.png")
+
+def send_image():
+    storage = Storage(client)
+    result = storage.create_file(open('optimized.png', 'rb'))
+
+get_image()
+
+try:
+    send_image()
+except Exception as e:
+    print(e)
