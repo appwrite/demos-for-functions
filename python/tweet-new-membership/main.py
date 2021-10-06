05ddaa1a6b28fa7870e962a15e8a72fd7799ae7f
Sunny Kushwaha <52101053+Ares358@users.noreply.github.com>
2021-10-06 06:31:00 +0530

    Add files via upload

--- python/tweet-new-membership/main.py
+++ python/tweet-new-membership/main.py
@@ -13,14 +13,8 @@ auth = tweepy.OAuthHandler(key, secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-# Load name of the new member
-payload = json.loads(os.environ['APPWRITE_FUNCTION_EVENT_DATA'])
-name = payload['name']
-
-
-
 def tweetNewMembership():
-    msg = 'Welcome! ' + name
+    msg = "A new user has joined a team!"
     api.update_status(msg)
     print('Tweet sent!')
 
