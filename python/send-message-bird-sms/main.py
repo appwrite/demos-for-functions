2b145310ba45718007d9f8f06f7e370c29b64d9f
Sarang S <eigengravy@protonmail.com>
2021-10-07 12:20:58 +0530

    fix: better logs

--- python/send-message-bird-sms/main.py
+++ python/send-message-bird-sms/main.py
@@ -2,19 +2,19 @@ import messagebird
 import json
 import os
 
-client = messagebird.Client(os.environ['MESSAGE_BIRD_KEY'])
+client = messagebird.Client(os.environ["MESSAGE_BIRD_KEY"])
 
 # parsing payload
-payload = json.loads(os.environ['APPWRITE_FUNCTION_DATA'])
-phoneNumber = payload['phoneNumber']
-text = payload['text']
-
+payload = json.loads(os.environ["APPWRITE_FUNCTION_DATA"])
+phoneNumber = payload["phoneNumber"]
+text = payload["text"]
+ 
 try:
     message = client.message_create(
-        os.environ['ORIGINATOR'],
+        os.environ["ORIGINATOR"],
         phoneNumber,
         text
     )
-    print(message.recipients.items[0].status)
+    print(message.recipients)
 except Exception as e:
     print(e)
