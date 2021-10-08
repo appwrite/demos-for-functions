a2ab238cf90b16841c5c962055e9dcc3dbcf4db2
Hanasagi <xenergymods@gmail.com>
2021-10-08 11:27:21 +0200

    feat: added compress tinypng image nodejs

--- nodejs/compress-tinypng-image/node_modules/node-appwrite/lib/exception.js
+++
@@ -1,9 +0,0 @@
-class AppwriteException extends Error {
-  constructor(message, code, response) {
-    super(message);
-    this.code = code;
-    this.response = response;
-  }
-}
-
-module.exports = AppwriteException;
