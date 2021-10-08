508a7e26039e19c62364e897cc7b9ca10f038613
Krish Gupta <krishguptadev@outlook.com>
2021-10-08 17:01:00 +0530

    feat: generateWebsiteScreenshot

---
+++ deno/generate-website-screenshot/index.js
@@ -0,0 +1,126 @@
+import * as sdk from "https://deno.land/x/appwrite/mod.ts";
+import * as dotenv from "https://deno.land/x/dotenv/mod.ts";
+import { download } from "https://deno.land/x/download/mod.ts";
+
+const env = dotenv.config();
+
+// Init SDK
+let client = new sdk.Client();
+client
+  .setEndpoint(env.APPWRITE_ENDPOINT)
+  .setProject(env.APPWRITE_PROJECT_ID) // Your project ID available by default
+  .setKey(env.APPWRITE_API_KEY); // Your secret API key
+
+// Initialise the storage SDK
+const storage = new sdk.Storage(client);
+
+// DATA
+const uri = "https://appwrite.io";
+const format = "png";
+
+async function generateWebsiteScreenshot(url, format = "jpg") {
+  const file = await createExportTask(await captureScreenshot(url, format));
+
+  const response = await fetch(file.url);
+
+  const blob = await response.blob();
+  const buffer = await blob.arrayBuffer();
+  const unit8arr = new Deno.Buffer(buffer).bytes();
+
+  await Deno.writeFile(file.filename, unit8arr);
+
+  console.log(await Deno.readFile(`./${file.filename}`));
+
+  const toBinString = (bytes) =>
+    bytes.reduce((str, byte) => str + byte.toString(2).padStart(8, "0"), "");
+
+  try {
+    const storedFile = await storage.createFile(
+      toBinString(await Deno.readFile(`./${file.filename}`))
+    );
+
+    console.log(`Stored screenshot with id: ${storedFile["$id"]}`);
+  } catch (error) {
+    console.error(error);
+  }
+
+  // Deno.remove(file.filename)
+}
+
+async function captureScreenshot(url, format) {
+  const config = JSON.stringify({ url, output_format: format });
+
+  // Create a request to capture the website screen shot
+
+  const captureRequest = await fetch(
+    "https://api.cloudconvert.com/v2/capture-website",
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${env.CLOUDCONVERT_API_KEY}`,
+        "Content-Type": "application/json",
+      },
+      body: config,
+    }
+  );
+
+  const captureRequestData = (await captureRequest.json()).data;
+
+  // Wait for the task to finish
+
+  await fetch(
+    `https://api.cloudconvert.com/v2/tasks/${captureRequestData.id}/wait`,
+    {
+      headers: {
+        Authorization: `Bearer ${env.CLOUDCONVERT_API_KEY}`,
+      },
+    }
+  );
+
+  return captureRequestData.id;
+}
+
+async function createExportTask(id) {
+  // Create a request to export the screenshot
+
+  const createExportRequest = await fetch(
+    `https://api.cloudconvert.com/v2/export/url?input=${id}`,
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${env.CLOUDCONVERT_API_KEY}`,
+      },
+    }
+  );
+
+  const createExportRequestData = (await createExportRequest.json()).data;
+
+  // Wait for it to get exported
+
+  await fetch(
+    `https://api.cloudconvert.com/v2/tasks/${createExportRequestData.id}/wait`,
+    {
+      headers: {
+        Authorization: `Bearer ${env.CLOUDCONVERT_API_KEY}`,
+      },
+    }
+  );
+
+  const captureDataRequest = await fetch(
+    `https://api.cloudconvert.com/v2/tasks/${id}`,
+    {
+      headers: {
+        Authorization: `Bearer ${env.CLOUDCONVERT_API_KEY}`,
+      },
+    }
+  );
+
+  const captureData = (await captureDataRequest.json()).data;
+
+  return {
+    url: `https://storage.cloudconvert.com/${createExportRequestData.id}/${captureData.result.files[0].filename}`,
+    filename: captureData.result.files[0].filename,
+  };
+}
+
+generateWebsiteScreenshot(uri, format);
