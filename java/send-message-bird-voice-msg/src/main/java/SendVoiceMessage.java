142dca1dbde11a5e630b9e0967157a3df32383c1
anshganatra <ansh.ganatra@somaiya.edu>
2021-10-09 18:08:32 +0530

    Added README.md

--- java/send-message-bird-voice-msg/src/main/java/SendVoiceMessage.java
+++ java/send-message-bird-voice-msg/src/main/java/SendVoiceMessage.java
@@ -18,9 +18,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SendVoiceMessage {
-    //fetch api key, phone number and message
-    //make api call
-    //return status
 
     public static String fetchEnvironmentVariable(String variable){
         return System.getenv(variable);
@@ -32,50 +29,37 @@ public class SendVoiceMessage {
         return json;
     }
 
-
-
     public static void main(String[] args) throws ParseException {
 
-//        String API_KEY = fetchEnvironmentVariable("MESSAGEBIRD_API_KEY");
-//
-//        String APPWRITE_FUNCTION_DATA = fetchEnvironmentVariable("APPWRITE_FUNCTION_DATA");
-//        JSONObject data = stringToJson(APPWRITE_FUNCTION_DATA);
-//        BigInteger phoneNumber = (BigInteger) data.get("phoneNumber");
-//        String text = (String) data.get("text");
+        String API_KEY = fetchEnvironmentVariable("MESSAGEBIRD_API_KEY");
 
-        String API_KEY = "iKS6mXNFYOeleTyIWvLl1SWBQ";
-
-        BigInteger phoneNumber =  new BigInteger("+918390390061");
-        String text = "Hello this is a test call from message bird";
+        String APPWRITE_FUNCTION_DATA = fetchEnvironmentVariable("APPWRITE_FUNCTION_DATA");
+        JSONObject data = stringToJson(APPWRITE_FUNCTION_DATA);
+        BigInteger phoneNumber = BigInteger.valueOf((Long) data.get("phoneNumber"));
+        String text = (String) data.get("text");
 
         if (API_KEY.isEmpty() || phoneNumber.equals(null) || text.isEmpty()) {
             System.out.println("Please specify your access key, one ore more phone numbers and a message body");
             return;
         }
 
-        // First create your service object
-        final MessageBirdService wsr = new MessageBirdServiceImpl(API_KEY);
+        // creation of service object
+        final MessageBirdService messageBirdService = new MessageBirdServiceImpl(API_KEY);
 
-        // Add the service to the client
-        final MessageBirdClient messageBirdClient = new MessageBirdClient(wsr);
+        // add the service to the client
+        final MessageBirdClient messageBirdClient = new MessageBirdClient(messageBirdService);
 
         try {
-            // Get Hlr using msgId and msisdn
-            System.out.println("Sending message:");
             final List<BigInteger> phones = new ArrayList<BigInteger>();
             phones.add(phoneNumber);
 
             // Send a voice message using the VoiceMessage object
-            final VoiceMessage vm = new VoiceMessage(text, phones);
-            vm.setIfMachine(IfMachineType.cont);
-            vm.setVoice(VoiceType.male);
-            vm.setRepeat(2);
-            final VoiceMessageResponse response = messageBirdClient.sendVoiceMessage(vm);
-            System.out.println(response.getRecipients().getItems().get(0).getStatus());
-            System.out.println(response.toString());
-
-//            final VoiceMessageResponse statusResponse = messageBirdClient.viewVoiceMessage(response.getId());
-//            System.out.println(response.toString());
+            final VoiceMessage voiceMessage = new VoiceMessage(text, phones);
+            voiceMessage.setIfMachine(IfMachineType.cont);
+            voiceMessage.setVoice(VoiceType.male);
+            voiceMessage.setRepeat(2);
+            final VoiceMessageResponse response = messageBirdClient.sendVoiceMessage(voiceMessage);
+            System.out.println("Status: " + response.getRecipients().getItems().get(0).getStatus());
 
         } catch (UnauthorizedException unauthorized) {
             if (unauthorized.getErrors() != null) {
