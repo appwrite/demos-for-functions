4d14cf8c9dec6a7a4e725db970381dc19f952f08
achintya <achintya22052000@gmail.com>
2021-10-09 13:26:56 +0530

    Update main.py

--- python/backup-to-dropbox/main.py
+++ python/backup-to-dropbox/main.py
@@ -1,6 +1,6 @@
+import sys
 import csv
 import os
-import sys
 
 import dropbox
 from dropbox.files import WriteMode
@@ -8,6 +8,31 @@ from dropbox.exceptions import ApiError, AuthError
 
 from appwrite.client import Client
 from appwrite.services.database import Database
+
+
+# Add OAuth2 access token here.
+# You can generate one for yourself in the App Console.
+# See <https://blogs.dropbox.com/developers/2014/05/generate-an-access-token-for-your-own-account/>
+# TOKEN = ""
+TOKEN = os.environ['DROPBOX_KEY']
+PATH1 = 'data_file.csv'
+PATH2 = 'data_file2.csv'
+BACKUPPATH = '/' + PATH1
+
+client = Client()
+client.set_endpoint(os.environ["APPWRITE_ENDPOINT"]) 
+client.set_project(os.environ["APPWRITE_FUNCTION_PROJECT_ID"]) # this is available by default.
+client.set_key(os.environ["APPWRITE_API_KEY"]) 
+
+# Create an instance of a Dropbox class, which can make requests to the API.
+print("Creating a Dropbox object...")
+with dropbox.Dropbox(TOKEN) as dbx:
+    # Check that the access token is valid.
+    try:
+        dbx.users_get_current_account()
+    except AuthError:
+        sys.exit("ERROR: Invalid access token; try re-generating an "
+            "access token from the app console on the web.")
 
 count2 = 0 # this will prevent the formation of repetative header files if the collection has more records than limit
 def JSON_to_CSV(list_db, filepath):
@@ -37,11 +62,14 @@ def JSON_to_CSV(list_db, filepath):
     data_file.close()
 
 # Uploads contents of PATH1 to Dropbox
-def backup():
+def backup(id):
     with open('data_file.csv', 'rb') as f:
+        FileName = "data_file" + '-' + id + ".csv"
+        BACKUPPATH = '/' + FileName
         # We use WriteMode=overwrite to make sure that the contents in the file are changed on upload
-        print("Uploading " + PATH1 + " to Dropbox as " + BACKUPPATH + "...")
+        print("Uploading " + FileName + " to Dropbox as " + BACKUPPATH + "...")
         try:
+            print(BACKUPPATH)
             dbx.files_upload(f.read(), BACKUPPATH, mode=WriteMode('overwrite'))
         except ApiError as err:
             # This checks for the specific error where a user doesn't have
@@ -55,63 +83,43 @@ def backup():
                 print(err)
                 sys.exit()
 
-
-# Add OAuth2 access token here.
-# You can generate one for yourself in the App Console.
-# See <https://blogs.dropbox.com/developers/2014/05/generate-an-access-token-for-your-own-account/>
-TOKEN = os.environ['DROPBOX_KEY']
-PATH1 = 'data_file.csv'
-PATH2 = 'data_file2.csv'
-BACKUPPATH = '/data_file1.csv'
-
-# Setup the appwrite SDK
-client = Client()
-client.set_endpoint(os.environ["APPWRITE_ENDPOINT"]) 
-client.set_project(os.environ["APPWRITE_FUNCTION_PROJECT_ID"]) # this is available by default.
-client.set_key(os.environ["APPWRITE_API_KEY"]) 
-
-
-# Provide the ID of the collection for making backup
-fileID = os.environ["COLLECTION_ID"]
-
 database = Database(client)
-limit = 100 # Max number of documents we can request at a time from appwrite.
-result = database.list_documents(fileID, limit = limit)
+result = database.list_collections()
 
-sum = result["sum"]
-j = int(sum/limit) # This will help us to run a loop for j number of times to get all the data from collection.
-data = result['documents']
-for doc in data:
-    keys = doc.keys() # Provides all the column names or keys of the document/database.
+collections = result['collections']
+collection_list = []
+name_list = []
+for i in range(result['sum']):
+    collection_list.append(collections[i]['$id'])
+    name_list.append(collections[i]['name'])
 
-JSON_to_CSV(result, PATH1) # We will atleast convert our data to .csv once.
+def all_stuff(file_id):
+    fileID = file_id
+    global PATH1
+    
+    limit = 100 # Max number of documents we can request at a time from appwrite.
+    result_dc = database.list_documents(fileID, limit = limit)
 
-if(j > 0): # If all the data can't be acquired from one request (more than 100 data in collection), then we will call it in batches.
-    for i in range(1, j): # As we have already requested for collection once already, we will start the loop from 1.
-        list = database.list_documents(fileID, limit=limit, offset=(i)*limit) 
-        JSON_to_CSV(list, PATH2)
-        data1 = open(PATH1, 'a+')        
-        data2 = open(PATH2, 'r')       
-        data1 = data1.write("\n" + data2.read())
+    sum = result_dc["sum"]
+    j = int(sum/limit) # This will help us to run a loop for j number of times to get all the data from collection.
 
+    JSON_to_CSV(result_dc, PATH1) # We will atleast convert our data to .csv once.
 
-# Check if we have the access token 
-if (len(TOKEN) == 0):
-    sys.exit("ERROR: Looks like you didn't add your access token. "
-        "Open up backup-and-restore-example.py in a text editor and "
-        "paste in your token in line 14.")
-    # Please consider looking into the README file and follow the steps to get the TOKEN.
-
-# Create an instance of a Dropbox class, which can make requests to the API.
-print("Creating a Dropbox object...")
-with dropbox.Dropbox(TOKEN) as dbx:
-    # Check that the access token is valid.
-    try:
-        dbx.users_get_current_account()
-    except AuthError:
-        sys.exit("ERROR: Invalid access token; try re-generating an "
-            "access token from the app console on the web.")
+    if(j > 0): # If all the data can't be acquired from one request (more than 100 data in collection), then we will call it in batches.
+        for i in range(1, j): # As we have already requested for collection once already, we will start the loop from 1.
+            list = database.list_documents(fileID, limit=limit, offset=(i)*limit) 
+            JSON_to_CSV(list, PATH2)
+            data1 = open(PATH1, 'a+')        
+            data2 = open(PATH2, 'r')       
+            data1 = data1.write("\n" + data2.read()) 
 
     # Create a backup of the current settings file.
-    backup()
-    print("Done!")
+    backup(fileID)
+
+
+for x in collection_list:
+    print(x)
+    all_stuff(x)
+    count2 = 0
+
+print("Done!")
