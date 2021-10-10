b12312f49161a6f514850558ff6a04590c47b447
Ganesh Tiwari <ganesht049@gmail.com>
2021-10-10 23:23:27 +0530

    initial commit

---
+++ dotnet/subscribe-to-sendgrid/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace subscribe_to_sendgrid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
