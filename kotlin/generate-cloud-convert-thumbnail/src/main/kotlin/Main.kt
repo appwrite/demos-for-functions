774cee9eea0ee553822a982fdc651d54aef4a90c
Hardik Sachan <sachanhardik@gmail.com>
2021-10-11 13:59:38 +0530

    generate thumbnail and get url of file

--- kotlin/generate-cloud-convert-thumbnail/src/main/kotlin/Main.kt
+++ kotlin/generate-cloud-convert-thumbnail/src/main/kotlin/Main.kt
@@ -1,7 +1,9 @@
+
 import com.cloudconvert.client.CloudConvertClient
 import com.cloudconvert.client.setttings.EnvironmentVariableSettingsProvider
+import com.cloudconvert.dto.request.CreateThumbnailsTaskRequest
 import com.cloudconvert.dto.request.UploadImportRequest
-import com.cloudconvert.dto.response.TaskResponse
+import com.cloudconvert.dto.request.UrlExportRequest
 import io.appwrite.Client
 import io.appwrite.services.Storage
 import java.io.InputStream
@@ -21,19 +23,53 @@ suspend fun main() {
         fileStream: InputStream
     ) = appwriteStorage.getFilenameAndInputStream(envVars.fileId)
 
-    val uploadTaskRequest = UploadImportRequest()
+    println("filename: $filename")
 
-    val uploadImportTaskResponse: TaskResponse = runCatching {
+    val createJobResponse = cloudConvertClient.jobs().create(
+        mapOf(
+            "import-file" to UploadImportRequest(),
+            "create-thumbnail" to CreateThumbnailsTaskRequest()
+                .setInput("import-file")
+                .setOutputFormat("png")
+                .set<CreateThumbnailsTaskRequest>("width", 400)
+                .set("height", 400),
+            "export-file" to UrlExportRequest().setInput("create-thumbnail")
+        )
+    ).body!!
+
+    val uploadTask = createJobResponse
+        .tasks
+        .stream()
+        .filter { it.name.equals("import-file") }.findFirst()
+        .get()
+
+    runCatching {
         cloudConvertClient
             .importUsing()
-            .upload(uploadTaskRequest, fileStream, filename)
+            .upload(uploadTask.id!!, uploadTask.result.form!!, fileStream, filename)
             .body!!
-    }.getOrThrow()
+    }.getOrElse {
+        System.err.println("Failed to upload/convert file")
+        throw(it)
+    }
 
-//    val uploadT
+    val waitJobResponse = cloudConvertClient
+        .jobs()
+        .wait(createJobResponse.id)
+        .body!!
 
+    val exportTaskMap =
+        waitJobResponse
+            .tasks
+            .stream()
+            .filter { it.name.equals("export-file") }.findFirst()
+            .get()
+            .result
+            .files[0]
 
-    println(filename)
+    val exportUrl = exportTaskMap["url"]!!
+    println(exportUrl)
+
     exitProcess(0)
 }
 
