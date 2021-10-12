6d9f92b2cada7e7c44fffa3a0d7d92b888092d46
Aditya Oberai <adityaoberai1@gmail.com>
2021-10-12 23:30:53 +0530

    feat(dotnet): Add SendSMS cloud function

---
+++ dotnet/send-sms/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Newtonsoft.Json;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+namespace SendSMS
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                string requestBody = Environment.GetEnvironmentVariable("APPWRITE_FUNCTION_DATA");
+                dynamic data = JsonConvert.DeserializeObject(requestBody);
+                string toPhoneNumber = data?.toPhoneNumber;
+                string messageBody = data?.messageBody;
+
+                TwilioClient.Init(Environment.GetEnvironmentVariable("TWILIO_ACCOUNTSID"),Environment.GetEnvironmentVariable("TWILIO_AUTHTOKEN"));
+
+                var message = MessageResource.Create(
+                    to: new PhoneNumber(toPhoneNumber),
+                    from: new PhoneNumber(Environment.GetEnvironmentVariable("TWILIO_PHONENUMBER")),
+                    body: messageBody
+                );
+
+                Console.WriteLine(message.Sid);
+            }
+            catch(Exception ex)
+            {
+                Console.Error.WriteLine(ex.Message);
+            }
+        }
+    }
+}
