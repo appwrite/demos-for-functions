26ce5a223e06d545c3278dfd9294f0a2457fde65
kinkurnia <p3kkiniekk@gmail.com>
2021-10-12 17:15:57 +0800

    add prune documents function

---
+++ deno/prune-documents/mod.ts
@@ -0,0 +1,38 @@
+import * as sdk from "https://deno.land/x/appwrite/mod.ts";
+
+// Init SDK
+const client: any = new sdk.Client();
+client
+    .setEndpoint(Deno.env.get("APPWRITE_ENDPOINT"))
+    .setKey(Deno.env.get("APPWRITE_API_KEY"))
+    .setProject(Deno.env.get("APPWRITE_FUNCTION_PROJECT_ID"))
+;
+
+const database: any = new sdk.Database(client);
+
+// 157784630 is 5 years in seconds
+let fiveYearsAgo: number = Date.now() - 157784630;
+
+let getCollections: any = await database.listCollections();
+let collections: Array<any> = getCollections.collections;
+
+for (let collection of collections){
+
+  let getDocuments: any = await database.listDocuments(
+    collection["$id"], 
+    [`createdAt<=${fiveYearsAgo}`],
+  );
+  let documents: Array<any> = getDocuments.documents;
+
+  for (let document of documents){
+    
+    await database.deleteDocument(
+      collection["$id"], 
+      document["$id"],
+    )
+    
+    console.log(`
+      Document ${document["$id"]} in collection ${collection["$id"]} deleted
+    `)
+  }
+}
