af44ad7bc96d0e7c80b7b739dfe17a9f3ef16ed9
Hardik Sachan <sachanhardik@gmail.com>
2021-10-12 20:46:58 +0530

    add logic to get gif url from GIPHY API

--- kotlin/generate-giphy-gif/build.gradle.kts
+++ kotlin/generate-giphy-gif/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.5.31"
+    kotlin("plugin.serialization") version "1.5.31"
 }
 
 group = "com.example"
@@ -28,4 +29,8 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0")
+    implementation("io.ktor:ktor-client-core:1.6.4")
+    implementation("io.ktor:ktor-client-cio:1.6.4")
+    implementation("io.ktor:ktor-client-serialization:1.6.4")
 }
--- kotlin/generate-giphy-gif/src/main/kotlin/Main.kt
+++ kotlin/generate-giphy-gif/src/main/kotlin/Main.kt
@@ -1,3 +1,48 @@
-fun main() {
-    println("Hello, World!")
+import io.ktor.client.*
+import io.ktor.client.request.*
+import model.Response
+import kotlin.system.exitProcess
+
+const val BASE_URL = "https://api.giphy.com/v1/gifs/search"
+const val GIPHY_API_KEY = "GIPHY_API_KEY"
+const val KEYWORD_KEY = "APPWRITE_FUNCTION_DATA"
+
+suspend fun main() {
+    val giphyApiKey = readStringFromEnvironment(GIPHY_API_KEY)
+    val keyword = readStringFromEnvironment(KEYWORD_KEY)
+
+    if (!isValidKeyword(keyword)) {
+        System.err.println("[ERR] `$keyword` is not valid")
+        exitProcess(-1)
+    }
+
+    val url = getGifUrlFor(
+        keyword = keyword,
+        apiKey = giphyApiKey
+    )
+
+    if (url == null) {
+        println("Sorry, No GIF found for $keyword")
+    } else {
+        println(url)
+    }
+
+    println("Powered By GIPHY")
+
+}
+
+suspend fun getGifUrlFor(
+    keyword: String,
+    apiKey: String
+): String? = HttpClient {
+    setupKotlinxSerializer()
+}.use { client ->
+
+    val response: Response = client.get(BASE_URL) {
+        parameter("api_key", apiKey)
+        parameter("q", keyword)
+        parameter("limit", 1)
+    }
+
+    response.gifs.firstOrNull()?.url
 }
