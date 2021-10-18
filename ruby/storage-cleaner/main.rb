b7377fce23245cd0b3da2d76c0863e64b35f85ea
Jacob Prud'homme <jacobprudhomme@users.noreply.github.com>
2021-10-18 02:27:16 -0400

    Created storage-cleaner example for Ruby

--- ruby/storage-cleaner/main.rb
+++ ruby/storage-cleaner/main.rb
@@ -1,15 +1,32 @@
 require 'appwrite'
 
-client = Appwrite::Client.new()
-
+# Set up client
+client = Appwrite::Client.new
 client
-    .set_endpoint('http://[HOSTNAME_OR_IP]/v1') # Your API Endpoint
-    .set_project(ENV.APPWRITE_FUNCTION_PROJECT_ID) # Your project ID available by default
-    .set_key('8d84bc37d4c59bb3b3e9d...a9f1ab664e94e6df53') # Your secret API key
-;
+    .set_endpoint(ENV['APPWRITE_ENDPOINT']) # Your API Endpoint
+    .set_project(ENV['APPWRITE_FUNCTION_PROJECT_ID']) # Your project ID available by default
+    .set_key(ENV['APPWRITE_API_KEY']) # Your secret API key
 
-storage = Appwrite::Storage.new(client);
+storage = Appwrite::Storage.new(client)
 
-response = storage.get_file(file_id: '[FILE_ID]');
+days_to_expire = ENV['DAYS_TO_EXPIRE'].to_i
+expiry_date = Time.now - (days_to_expire * 24 * 60 * 60) # Converting days into seconds
 
-puts response
+num_deleted = 0
+begin
+    files_in_storage = storage.list_files(limit: 100)
+
+    files_in_storage['files'].each do |file|
+        # Delete file if it's expired
+        next unless file['dateCreated'] < expiry_date.to_i
+
+        storage.delete_file(file_id: file['$id'])
+        num_deleted += 1
+
+        puts "Deleted file #{file['name']}"
+    end
+rescue Appwrite::Exception => e
+    puts e.message
+end
+
+puts "Total files deleted: #{num_deleted}"
