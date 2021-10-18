6ea351f1fc1d4ce820fe2c6a490cd7eea60de897
Aditya Rana <official.adityarana01@gmail.com>
2021-10-18 05:40:53 +0530

    Created tasks and job to be done by cloudconvert

--- deno/optimize-cloud-convert-file/index.js
+++ deno/optimize-cloud-convert-file/index.js
@@ -9,3 +9,21 @@ client
   .setEndpoint(Deno.env.get("APPWRITE_API_ENDPOINT")) // Your API Endpoint
   .setProject(Deno.env.get("APPWRITE_PROJECT_ID")) // Your project ID
   .setKey(Deno.env.get("APPWRITE_API_KEY")); // Your secret API key
+
+const jobs = {
+  tasks: {
+    "upload-file": {
+      operation: "import/upload",
+    },
+    "optimize-file": {
+      operation: "optimize",
+      input: ["upload-file"],
+    },
+    "optimized-file-url": {
+      operation: "export/url",
+      input: ["optimize-file"],
+      inline: false,
+      archive_multiple_files: false,
+    },
+  },
+};
