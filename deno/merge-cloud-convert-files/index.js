7207935309fbe1809f21aa8efde0bd766a515404
Aditya Rana <official.adityarana01@gmail.com>
2021-10-19 03:38:49 +0530

    Adding upload tasks dynamically

--- deno/merge-cloud-convert-files/index.js
+++ deno/merge-cloud-convert-files/index.js
@@ -11,3 +11,37 @@ client
   .setEndpoint(Deno.env.get("APPWRITE_API_ENDPOINT")) // Your API Endpoint
   .setProject(Deno.env.get("APPWRITE_PROJECT_ID")) // Your project ID
   .setKey(Deno.env.get("APPWRITE_API_KEY")); // Your secret API key
+
+const { ids } = JSON.parse(Deno.env.get("APPWRITE_FUNCTION_DATA"));
+
+const addUploadTasks = (jobs) => {
+  for (let fileCount = 0; fileCount < ids.length; fileCount++) {
+    const uploadTaskName = "upload-file-" + (ids.length - fileCount);
+    jobs.tasks = {
+      [uploadTaskName]: { operation: "import/upload" },
+      ...jobs.tasks,
+    };
+    jobs.tasks["merge-files"].input.push(uploadTaskName);
+  }
+  return jobs;
+};
+
+const jobs = {
+  tasks: {
+    "merge-files": {
+      operation: "merge",
+      output_format: "pdf",
+      engine: "poppler",
+      input: [],
+    },
+    "merged-files-url": {
+      operation: "export/url",
+      input: ["merge-files"],
+      inline: false,
+      archive_multiple_files: false,
+    },
+  },
+};
+
+addUploadTasks(jobs);
+console.log(jobs);
