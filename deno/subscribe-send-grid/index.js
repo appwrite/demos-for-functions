16a7cb30d21196fbe6eec950ee66fc225cebda53
Aditya Rana <official.adityarana01@gmail.com>
2021-10-19 16:54:08 +0530

    Connecting to appwrite and setting env variables

---
+++ deno/subscribe-send-grid/index.js
@@ -0,0 +1,14 @@
+import * as sdk from "https://deno.land/x/appwrite@0.4.1/mod.ts";
+import { config } from "https://deno.land/x/dotenv@v3.0.0/mod.ts";
+
+// Export dotenv file to Deno.env
+config({ export: true });
+
+const client = new sdk.Client();
+
+client
+  .setEndpoint(Deno.env.get("APPWRITE_API_ENDPOINT")) // Your API Endpoint
+  .setProject(Deno.env.get("APPWRITE_PROJECT_ID")) // Your project ID
+  .setKey(Deno.env.get("APPWRITE_API_KEY")); // Your secret API key
+
+const { email } = JSON.parse(Deno.env.get("APPWRITE_FUNCTION_DATA"));
