84f80b054a8d8a50887d6015be37ee0813ecd7e8
Shylajhaa <rsshylu@hotmail.com>
2021-10-20 00:32:58 +0530

    Issue fixes

--- kotlin/subscribe-to-sendgrid/src/main/kotlin/SendgridSubscription.kt
+++ kotlin/subscribe-to-sendgrid/src/main/kotlin/SendgridSubscription.kt
@@ -5,37 +5,41 @@ import com.sendgrid.Request
 import com.sendgrid.SendGrid
 import java.io.IOException
 
-fun main(args: Array<String>) {
-    val sendgrid = SendGrid(System.getenv("SENDGRID_API_KEY"))
-    val request = Request()
+class SendgridSubscription {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val sendgrid = SendGrid(System.getenv("SENDGRID_API_KEY"))
+            val request = Request()
 
-    val funcData = Gson().fromJson<Map<String, String>>(
-        System.getenv("APPWRITE_FUNCTION_DATA"),
-        object : TypeToken<Map<String, String>>(){}.type
-    )
+            val funcData = Gson().fromJson<Map<String, String>>(
+                System.getenv("APPWRITE_FUNCTION_DATA"),
+                object : TypeToken<Map<String, String>>(){}.type
+            )
 
-    println(funcData)
+            if (funcData == null) {
+                println("Empty arguments")
+            }
+            val email = funcData["email"] ?: throw Exception("Invalid arguments")
+            val sendgridSubscriptionRequest = SendgridSubscriptionRequest(
+                listOf(
+                    Contact(email = email)
+                )
+            )
 
-    val email = funcData["email"] ?: throw Exception("Invalid arguments")
-    val sendgridSubscriptionRequest = SendgridSubscriptionRequest(
-        listOf(
-            Contact(email = email)
-        )
-    )
+            try {
+                request.method = Method.PUT
+                request.endpoint = "marketing/contacts"
+                request.body = Gson().toJson(sendgridSubscriptionRequest)
 
-    println(email)
-    try {
-        request.method = Method.PUT
-        request.endpoint = "marketing/contacts"
-        request.body = Gson().toJson(sendgridSubscriptionRequest)
+                val response = sendgrid.api(request)
 
-        val response = sendgrid.api(request)
-
-        println(response)
-        if (response.statusCode >= 200) {
-            println("Successfully subscribed $email to Sendgrid")
+                if (response.statusCode >= 200) {
+                    println("Successfully subscribed $email to Sendgrid")
+                }
+            } catch (ex: IOException) {
+                throw ex
+            }
         }
-    } catch (ex: IOException) {
-        throw ex
     }
 }
