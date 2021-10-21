c2a9d78e043d181ed3a6f984234f5ac021e1fb64
JeisonSanches <JeisonSanches@users.noreply.github.com>
2021-10-20 23:39:32 -0300

    implement subscribe-to-mailchimp

---
+++ dart/subscribe-to-mailchimp/main.dart
@@ -0,0 +1,42 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+
+main(List<String> args) async {
+  Map<String, String> envVars = Platform.environment;
+
+  // Assert environment variables are set
+  if (envVars["APPWRITE_MAILCHIMP_API_KEY"] == null)
+    throw Exception("APPWRITE_MAILCHIMP_API_KEY is not defined.");
+  if (envVars["APPWRITE_MAILCHIMP_LIST_ID"] == null)
+    throw Exception("APPWRITE_MAILCHIMP_LIST_ID is not defined.");
+  if (envVars["APPWRITE_FUNCTION_DATA"] == null)
+    throw Exception("You need to pass an email.");
+
+  // Get the API parameters from Appwrite's environment variable
+  final String apiKey = envVars['APPWRITE_MAILCHIMP_API_KEY']!;
+  final String listId = envVars['APPWRITE_MAILCHIMP_LIST_ID']!;
+  final String email = envVars['APPWRITE_FUNCTION_DATA']!;
+  final String dc = apiKey.split('-').last;
+
+  // constants
+  final defaultStatus = 'subscribed';
+  final String url = 'https://$dc.api.mailchimp.com/3.0/lists/$listId/members';
+
+  final body = {'email_address': email, 'status': defaultStatus};
+  final headers = {
+    'Authorization': 'apikey $apiKey',
+    'Content-Type': 'application/json'
+  };
+
+  try {
+    var response = await http.post(
+      Uri.parse(url),
+      headers: headers,
+      body: jsonEncode(body),
+    );
+    print(response.body);
+  } catch (e) {
+    print(e);
+  }
+}
