34982a7a064085fd715c809509dbc3d259729eea
Hardik Sachan <sachanhardik@gmail.com>
2021-10-25 23:53:05 +0530

    save compressed image to storage

--- kotlin/compress-tinypng-image/src/main/kotlin/Main.kt
+++ kotlin/compress-tinypng-image/src/main/kotlin/Main.kt
@@ -1,20 +1,20 @@
 import io.appwrite.Client
 import io.appwrite.services.Storage
 import io.ktor.client.*
+import io.ktor.client.call.*
 import io.ktor.client.features.json.*
 import io.ktor.client.features.json.serializer.*
 import io.ktor.client.request.*
+import io.ktor.client.statement.*
 import io.ktor.http.*
-import io.ktor.util.*
-import io.ktor.utils.io.core.*
+import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
-import models.TinypngOutput
+import models.AppwriteFile
 import models.TinypngRequest
 import models.TinypngResponse
 import utils.Env
+import java.io.File
 import java.util.*
-import kotlin.io.use
-import kotlin.text.toByteArray
 
 private const val IMAGE_URL_KEY = "APPWRITE_FUNCTION_DATA"
 private const val TINYPNG_API_KEY = "TINYPNG_API_KEY"
@@ -25,25 +25,34 @@ private const val APPWRITE_SECRET_KEY = "APPWRITE_API_KEY"
 private const val TINYPNG_ENDPOINT = "https://api.tinify.com/shrink"
 
 suspend fun main() {
-//    val appwriteClient = Client()
-//        .setEndpoint(Env.readString(APPWRITE_ENDPOINT_KEY))
-//        .setProject(Env.readString(APPWRITE_PROJECT_ID_KEY))
-//        .setKey(Env.readString(APPWRITE_SECRET_KEY))
-//
-//    val appwriteStorage = Storage(appwriteClient)
+    val appwriteClient = Client()
+        .setEndpoint(Env.readString(APPWRITE_ENDPOINT_KEY))
+        .setProject(Env.readString(APPWRITE_PROJECT_ID_KEY))
+        .setKey(Env.readString(APPWRITE_SECRET_KEY))
+
+    val appwriteStorage = Storage(appwriteClient)
 
     val jsonParser = Json {
         isLenient = true
         ignoreUnknownKeys = true
     }
 
-    println(compressImage(
-        imageUrl = Env.readString(IMAGE_URL_KEY),
+    val baseImageUrl = Env.readString(IMAGE_URL_KEY)
+
+    val compressedImageUrl = compressImage(
+        imageUrl = baseImageUrl,
         endpoint = TINYPNG_ENDPOINT,
         apiKey = Env.readString(TINYPNG_API_KEY),
         jsonParser = jsonParser
-    ))
+    )
 
+    val compressedImage = appwriteStorage.createFromUrl(
+        filename = "compressed-$baseImageUrl",
+        url = compressedImageUrl,
+        jsonParser
+    )
+
+    println(compressedImage.id)
 }
 
 suspend fun compressImage(
@@ -68,3 +77,20 @@ suspend fun compressImage(
 
 fun authHeaderValueFor(apiKey: String): String =
     "Basic ${Base64.getEncoder().encodeToString("api:$apiKey".toByteArray())}"
+
+suspend fun Storage.createFromUrl(filename: String, url: String, jsonParser: Json): AppwriteFile {
+    val file = File(filename)
+
+    HttpClient().use { httpClient ->
+        val httpResponse: HttpResponse = httpClient.get(url)
+        val responseBody: ByteArray = httpResponse.receive()
+        file.writeBytes(responseBody)
+    }
+
+    val appwriteFile: AppwriteFile = createFile(file).body?.string()!!.run {
+        jsonParser.decodeFromString(this)
+    }
+
+    file.deleteOnExit()
+    return appwriteFile
+}
