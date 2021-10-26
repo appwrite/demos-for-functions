6552bcb41a2dc6c3dc7e5f773dcb512d897dcd1c
Kinie K Kusuma <66422665+kinkusuma@users.noreply.github.com>
2021-10-26 19:29:58 +0800

    Update mod.ts

--- deno/prune-documents/mod.ts
+++ deno/prune-documents/mod.ts
@@ -3,9 +3,9 @@ import * as sdk from "https://deno.land/x/appwrite/mod.ts";
 // Init SDK
 const client: any = new sdk.Client();
 client
-    .setEndpoint(Deno.env.get("APPWRITE_ENDPOINT"))
-    .setKey(Deno.env.get("APPWRITE_API_KEY"))
-    .setProject(Deno.env.get("APPWRITE_FUNCTION_PROJECT_ID"))
+  .setEndpoint(Deno.env.get("APPWRITE_ENDPOINT"))
+  .setKey(Deno.env.get("APPWRITE_API_KEY"))
+  .setProject(Deno.env.get("APPWRITE_FUNCTION_PROJECT_ID"))
 ;
 
 const database: any = new sdk.Database(client);
@@ -13,26 +13,55 @@ const database: any = new sdk.Database(client);
 // 157784760000 is 5 years in miliseconds
 let fiveYearsAgo: number = Date.now() - 157784760000;
 
-let getCollections: any = await database.listCollections();
-let collections: Array<any> = getCollections.collections;
+const collections = [];
+let offset = 0;
+while (true){
+  let response: any = await database.listCollections(
+    undefined,
+    10,
+    offset
+  );
+
+  collections.push(...response.collections);
+
+  if ((response.collections).length < 1){
+    break
+  }
+
+  offset += 10;
+}
+offset = 0;
 
 for (let collection of collections){
+  let collectionId = collection["$id"];
 
-  let getDocuments: any = await database.listDocuments(
-    collection["$id"], 
-    [`createdAt<=${fiveYearsAgo}`],
-  );
-  let documents: Array<any> = getDocuments.documents;
+  const documents = [];
+  while (true){
+    let response: any = await database.listDocuments(
+      collectionId,
+      [`createdAt<=${fiveYearsAgo}`],
+      10,
+      offset
+    );
+
+    documents.push(...response.documents);
+    
+    if ((response.documents).length < 1){
+      break
+    }
+
+    offset += 10;
+  }
+  offset = 0;
+
+  if (documents.length < 1){
+    continue
+  }
 
   for (let document of documents){
-    
-    await database.deleteDocument(
-      collection["$id"], 
+    console.log(await database.deleteDocument(
+      collectionId, 
       document["$id"],
-    )
-    
-    console.log(`
-      Document ${document["$id"]} in collection ${collection["$id"]} deleted
-    `)
+    ));
   }
 }
