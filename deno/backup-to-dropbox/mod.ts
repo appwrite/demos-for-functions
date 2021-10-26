d74fade7ca3d4ae3013916db6d9a922f5949696d
Kinie K Kusuma <66422665+kinkusuma@users.noreply.github.com>
2021-10-26 19:26:22 +0800

    Update mod.ts

--- deno/backup-to-dropbox/mod.ts
+++ deno/backup-to-dropbox/mod.ts
@@ -8,77 +8,96 @@ client
   .setProject(Deno.env.get("APPWRITE_FUNCTION_PROJECT_ID"))
 ;
 
-const database: any = new sdk.Database(client);
 const accessToken = Deno.env.get("DROPBOX_ACCESS_TOKEN");
+const database: any = new sdk.Database(client);
 
-const getCollections = async (): Promise<any> =>{
-  let response: any = await database.listCollections();
-  return response.collections;
-} 
+const collections = [];
+let offset = 0;
+while (true){
+  let response: any = await database.listCollections(
+    undefined,
+    10,
+    offset
+  );
 
-const getDocuments = async (collectionId: string): Promise<any> =>{
-  let output = "";
-  let response: any = await database.listDocuments(collectionId);
-  let documents: Array<any> = response.documents;
+  collections.push(...response.collections);
+
+  if ((response.collections).length < 1){
+    break
+  }
+
+  offset += 10;
+}
+offset = 0;
+
+let data = "";
+for (let collection of collections){
+  let collectionId = collection["$id"];
+  let collectionName = collection["name"];
+
+  const documents = [];
+  while (true){
+    let response: any = await database.listDocuments(
+      collectionId,
+      undefined,
+      10,
+      offset
+    );
+
+    documents.push(...response.documents);
+    
+    if ((response.documents).length < 1){
+      break
+    }
+
+    offset += 10;
+  }
+  offset = 0;
+
+  if (documents.length < 1){
+    continue
+  }
+
+
+  for (let attr of Object.keys(documents[0])){
+    data += `${collectionName}.${attr},`;
+  }
+  data += "\n"
+
   for (let document of documents){
       // if this doesnt use json.stringify the value will be: [object object]
       // and the comma replaced to dot because the value will be splited in csv
       document["$permissions"] = JSON.stringify(document["$permissions"]).replace(',', '.')
       
-      for (let data of Object.values(document)){
-        output += data + ",";
+      for (let item of Object.values(document)){
+        data += item + ",";
       }
 
-      output += "\n"
+      data += "\n"
   }
-  return output;
-} 
-
-const getDocumentsAttr = async (collectionId: string): Promise<any> => {
-  let output = "";
-  let response: any = await database.listDocuments(collectionId);
-  let document: Array<any> = response.documents[0];
-  for (let attr of Object.keys(document)){
-    output += attr + ",";
-  }
-  return output
-} 
-
-let data = "";
-const date = Date.now();
-const collections: any = await getCollections();
-
-for (let collection of collections){
-
-  let collectionId = collection["$id"];
-  let collectionName = collection["name"];
-
-  let fileName = `${collectionName}-${date}.csv`;
-
-  data += await getDocumentsAttr(collectionId);
   data += "\n"
-  data += await getDocuments(collectionId);
+}
 
-  const response = await fetch(
-    "https://content.dropboxapi.com/2/files/upload",
-    {
-      method: "POST",
-      headers: {
-        "Authorization":
-          `Bearer ${accessToken}`,
-        "Dropbox-API-Arg":
-          `{"path": "/backup/${fileName}","mode": "add","autorename": true,"mute": false,"strict_conflict": false}`,
-        "Content-Type": "application/octet-stream",
-      },
-      body: new TextEncoder().encode(data),
+const date = Date.now();
+const fileName = `${Deno.env.get("APPWRITE_FUNCTION_PROJECT_ID")}-${date}.csv`;
+
+const response = await fetch(
+  "https://content.dropboxapi.com/2/files/upload",
+  {
+    method: "POST",
+    headers: {
+      "Authorization":
+        `Bearer ${accessToken}`,
+      "Dropbox-API-Arg":
+        `{"path": "/backup/${fileName}","mode": "add","autorename": true,"mute": false,"strict_conflict": false}`,
+      "Content-Type": "application/octet-stream",
     },
-  );
+    body: new TextEncoder().encode(data),
+  },
+);
 
-  if (response['status'] === 200){
-    console.log(`Success backup collection ${collectionId} to ${fileName}`);
-  } else {
-    console.log(response['statusText'])
-  }
-
-  data = "";
+if (response['status'] === 200){
+  console.log(`Success backup collection to ${fileName}`);
+} else {
+  console.error(response['statusText'])
 }
