89d8a82fc3b9e7857270e231a0c261e34441aae4
Hardik Sachan <sachanhardik@gmail.com>
2021-10-27 22:40:51 +0530

    Added logic to tweet

--- kotlin/tweet-new-membership/src/main/kotlin/Main.kt
+++ kotlin/tweet-new-membership/src/main/kotlin/Main.kt
@@ -1,3 +1,42 @@
+import io.github.redouane59.twitter.TwitterClient
+import io.github.redouane59.twitter.dto.tweet.Tweet
+import io.github.redouane59.twitter.signature.TwitterCredentials
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import models.Membership
+import utils.Env
+import kotlin.system.exitProcess
+
+private const val APPWRITE_FUNCTION_EVENT_DATA = "APPWRITE_FUNCTION_EVENT_DATA"
+
+private const val TWITTER_API_KEY = "TWITTER_API_KEY"
+private const val TWITTER_API_KEY_SECRET = "TWITTER_API_KEY_SECRET"
+private const val TWITTER_ACCESS_TOKEN = "TWITTER_ACCESS_TOKEN"
+private const val TWITTER_ACCESS_TOKEN_SECRET = "TWITTER_ACCESS_TOKEN_SECRET"
+
 fun main() {
-    println("Hello, world")
+    val jsonParser = Json {
+        isLenient = true
+        ignoreUnknownKeys = true
+    }
+
+    val membership: Membership = Env.read(APPWRITE_FUNCTION_EVENT_DATA) { jsonParser.decodeFromString(it) }
+
+    val tweet = tweetMembership(membership)
+    println(tweet.id)
+
+    exitProcess(0)
+}
+
+fun tweetMembership(membership: Membership): Tweet {
+    val twitterClient = TwitterClient(
+        TwitterCredentials.builder()
+            .accessToken(Env.readString(TWITTER_ACCESS_TOKEN))
+            .accessTokenSecret(Env.readString(TWITTER_ACCESS_TOKEN_SECRET))
+            .apiKey(Env.readString(TWITTER_API_KEY))
+            .apiSecretKey(Env.readString(TWITTER_API_KEY_SECRET))
+            .build()
+    )
+
+    return twitterClient.postTweet(membership.tweetText)
 }
