caa99e1f5c2362d13bd4134df76bc88925315800
TafadzwaD <tafadzwademba@gmail.com>
2021-10-30 14:07:25 +0200

    Switched to http package for http request as dio was resulting in a larger package size, error uploading

--- dart/get-stripe-payment-status/main.dart
+++ dart/get-stripe-payment-status/main.dart
@@ -1,20 +1,30 @@
 import 'dart:io';
 import 'dart:convert';
+import 'package:dio/dio.dart';
 
 void main() async {
 Map<String, String> environmentVariables = Platform.environment;
-  final payload = jsonDecode(environmentVariables['APPWRITE_FUNCTION_DATA']);
 
 
-  final String stripePaymentId = payload['stripePaymentId'];
+  final String stripeKey = environmentVariables['STRIPE_KEY'];
+  final String stripePaymentId = environmentVariables['APPWRITE_FUNCTION_DATA'];
+
+Map<String,dynamic> header = {
+     'headers': {
+         'Authorization': 'Bearer $stripeKey',
+     },
+ };
+
+  final String url = 'https://api.stripe.com/v1/payment_intents/$stripePaymentId';
 
  
+Dio().options.headers['Authorization'] = "Bearer " + stripeKey;
 
-
-  try{
-
-  }catch(error){
-    print('Error fetching status $error');
+  try {
+    var response = await Dio().get(url);
+    print(response); // respomse
+  } catch (e) {
+    print('Error retrieving status $e');
   }
 
 }
