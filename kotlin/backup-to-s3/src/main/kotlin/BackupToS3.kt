615d4917300e9c0a07a7ae4a5a68a9be550bf0f7
Shylajhaa <rsshylu@hotmail.com>
2021-10-30 15:02:12 +0530

    Change storage to AWS S3

--- kotlin/backup-to-s3/src/main/kotlin/BackupToS3.kt
+++ kotlin/backup-to-s3/src/main/kotlin/BackupToS3.kt
@@ -1,14 +1,17 @@
+import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.services.s3.AmazonS3Client
+import com.amazonaws.services.s3.S3ClientOptions
+import com.amazonaws.services.s3.model.PutObjectRequest
 import com.google.gson.Gson
 import io.appwrite.Client
 import io.appwrite.services.Database
-import io.appwrite.services.Storage
 import kotlinx.coroutines.runBlocking
 import java.io.File
 import java.io.FileWriter
 import kotlin.system.exitProcess
 
 
-class BackupToStorage {
+class BackupToS3 {
     companion object {
         private var collectionsCsv: FileWriter? = null
         private const val headers = "collection_id, permissions, name, date_created, date_updated, rules"
@@ -16,11 +19,37 @@ class BackupToStorage {
         @JvmStatic
         fun main(args: Array<String>) {
             try {
-                val appWriteclient = Client()
-                    .setEndpoint(System.getenv("APPWRITE_ENDPOINT"))
-                    .setProject(System.getenv("APPWRITE_FUNCTION_PROJECT_ID"))
-                    .setKey(System.getenv("APPWRITE_API_KEY"))
+//                val appWriteclient = Client()
+//                    .setEndpoint(System.getenv("APPWRITE_ENDPOINT"))
+//                    .setProject(System.getenv("APPWRITE_FUNCTION_PROJECT_ID"))
+//                    .setKey(System.getenv("APPWRITE_API_KEY"))
 
+                val appWriteclient = Client()
+                    .setEndpoint("http://localhost:90/v1")
+                    .setProject("6162ef5ab6c11")
+                    .setKey("5b3a9d2401f6648ca3535a2d89be5dd941758479f29e515ed4958978b232b5f2b78af430eab443dcfd58db90c1c134e6b97f626795f6de346923c513b392cd7b23b0b4edbcafd1522055cae7670ffe1bfe97cbc6a235aeaf7a829b7d93acc1ceb293c5d753393060ed2e4f0778d6901edde97fa0b326beb91e9159d25c49afd5")
+
+//                val s3 = AmazonS3Client(BasicAWSCredentials(
+//                    System.getenv("AWS_API_KEY"),
+//                    System.getenv("AWS_API_SECRET"))).apply {
+//                        setEndpoint("s3.amazonaws.com").apply {
+//                            println("S3 endpoint is s3.amazonaws.com")
+//                        }
+//                        setS3ClientOptions(
+//                            S3ClientOptions.builder()
+//                                .setPathStyleAccess(true).build()
+//                        )
+//                    }
+                val s3 = AmazonS3Client(BasicAWSCredentials(
+                    "AKIAUZ7FHRINECJWRA3Y",
+                    "kZpa51dGsa4BVo6W0+18LscmM5MRl/q2oGmhxgLj")
+                ).apply {
+                    setEndpoint("s3.amazonaws.com")
+                    setS3ClientOptions(
+                        S3ClientOptions.builder()
+                            .setPathStyleAccess(true).build()
+                    )
+                }
 
                 collectionsCsv = FileWriter("${System.getProperty("user.dir")}/collections.csv")
                 if (collectionsCsv == null) {
@@ -55,14 +84,17 @@ class BackupToStorage {
                     collectionsCsv!!.append(collection.rules.toString())
                     collectionsCsv!!.append("\n")
                 }
-                val storage = Storage(appWriteclient)
 
-                val storageResponse = runBlocking {
-                    storage.createFile(File("${System.getProperty("user.dir")}/collections.csv"))
-                }
+                val file = File("${System.getProperty("user.dir")}/collections.csv")
 
-                if (storageResponse.code >= 200) {
-                    println("Successfully uploaded collections.csv to storage")
+                val s3response = s3.putObject(PutObjectRequest(
+                    "awhacktoberfest",
+                    "collections",
+                    file
+                ))
+
+                if (s3response != null) {
+                    println("Successfully uploaded collections.csv to S3")
                 }
             } catch (e: Exception) {
                 println(e)
