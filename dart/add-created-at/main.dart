b3e63b16c444f77650f77cd424bdf6d1eda13aa4
TafadzwaD <tafadzwademba@gmail.com>
2021-10-30 09:57:20 +0200

    Get documentId and collectionId from function event data which is an environment variable

--- dart/add-created-at/main.dart
+++ dart/add-created-at/main.dart
@@ -1,7 +1,22 @@
 import 'package:dart_appwrite/dart_appwrite.dart';
 import 'dart:io';
+import 'dart:convert';
 
 void main() async {
-Map<String, String> enviromentVariables = Platform.environment;
+Map<String, String> environmentVariables = Platform.environment;
+final Client client = Client();
+client
+.setEndpoint(environmentVariables['APPWRITE_ENDPOINT'])
+.setProject(environmentVariables['APPWRITE_PROJECT_ID'])
+.setKey(environmentVariables['API_KEY']);
+
+final db = Database(client); //initialising database
+
+// Get the collectionID and also the ID of the newly created document from Appwrite's environment variable
+  final payload = jsonDecode(environmentVariables['APPWRITE_FUNCTION_EVENT_DATA']);
+
+
+  final String documentId = payload['\$id'];
+  final String collectionId = payload['\$collection'];
 
 }
